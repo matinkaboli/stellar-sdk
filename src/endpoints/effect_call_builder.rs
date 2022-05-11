5333c42e273ffd279f5a50e57d3111ebd0666d65
Matin Kaboli <matinkaboli@aol.com>
2022-05-12 03:22:29 +0430

    fix: finalizing effect_call_builder

---
+++ src/endpoints/effect_call_builder.rs
@@ -0,0 +1,95 @@
+use crate::endpoints::{call_builder::CallBuilder, Effect, Record, Server};
+use crate::utils::{req, Direction, Endpoint};
+
+#[derive(Debug)]
+pub struct EffectCallBuilder<'a> {
+    pub server: &'a Server,
+    pub cursor: Option<String>,
+    pub order: Option<Direction>,
+    pub limit: Option<u8>,
+    pub endpoint: Endpoint,
+}
+
+impl<'a> CallBuilder<'a, Effect> for EffectCallBuilder<'a> {
+    fn new(s: &'a Server) -> Self {
+        Self {
+            server: s,
+            cursor: None,
+            order: None,
+            limit: None,
+            endpoint: Endpoint::None,
+        }
+    }
+
+    fn cursor(&mut self, cursor: &str) -> &mut Self {
+        self.cursor = Some(String::from(cursor));
+
+        self
+    }
+
+    fn order(&mut self, dir: Direction) -> &mut Self {
+        self.order = Some(dir);
+
+        self
+    }
+
+    fn limit(&mut self, limit_number: u8) -> &mut Self {
+        self.limit = Some(limit_number);
+
+        self
+    }
+
+    fn for_endpoint(&mut self, endpoint: Endpoint) -> &mut Self {
+        self.endpoint = endpoint;
+
+        self
+    }
+
+    fn call(&self) -> Result<Record<Effect>, &str> {
+        let mut url = String::from(format!(
+            "{}{}{}",
+            &self.server.0,
+            self.endpoint.as_str(),
+            "/effects?",
+        ));
+
+        if let Some(x) = &self.cursor {
+            url.push_str(&format!("&cursor={}", x));
+        }
+
+        if let Some(x) = &self.order {
+            url.push_str(&format!("&order={}", x.as_str()));
+        }
+
+        if let Some(x) = &self.limit {
+            url.push_str(&format!("&limit={}", x));
+        }
+
+        let resp = req(&url);
+
+        match resp {
+            Ok(d) => {
+                let p: Record<Effect> = serde_json::from_str(&d).unwrap();
+
+                Ok(p)
+            }
+            Err(_) => Err("Error while fetching data from horizon."),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn assets_horizon_test() {
+        let s = Server::new(String::from("https://horizon.stellar.org"));
+
+        let mut ecb = EffectCallBuilder::new(&s);
+
+        let effect_records = ecb.limit(200).call().unwrap();
+
+        assert_eq!(effect_records._embedded.records.len(), 200);
+    }
+}
