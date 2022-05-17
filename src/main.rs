04df1010cdb6819b29d45009d5a72b91d7eadc1b
Matin Kaboli <matinkaboli@aol.com>
2022-05-17 14:30:16 +0430

    fix: create a lib file

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,12 @@
-mod endpoints;
-mod types;
-mod utils;
-
-use endpoints::{CallBuilder, Server};
-use types::Asset;
-use utils::Endpoint;
+use stellar_sdk::endpoints::Server;
 
 fn main() {
-    let horizon = String::from("https://horizon.stellar.org");
-    let server = Server::new(horizon);
+    let s = String::from("https://horizon.stellar.org");
+    let s = Server::new(s);
 
-    let rbt = Asset::new(
-        "RBT",
-        "GCMSCRWZ3QBOI6AF75B5ZWDBXOSMIRW4FSBZH5OI65Y4H4GVH7LPSOYS",
-    );
+    let acc = s
+        .load_account("GAUZUPTHOMSZEV65VNSRMUDAAE4VBMSRYYAX3UOWYU3BQUZ6OK65NOWM")
+        .unwrap();
 
-    let _offers = server
-        .offers()
-        .limit(2)
-        .for_endpoint(Endpoint::Accounts(String::from(
-            "GAUZUPTHOMSZEV65VNSRMUDAAE4VBMSRYYAX3UOWYU3BQUZ6OK65NOWM",
-        )))
-        .selling(&rbt)
-        .call();
-
-    let an = server.transactions().limit(2).call().unwrap();
-
-    println!("{:#?}", an);
+    println!("{:#?}", acc);
 }
