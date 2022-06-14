647a27a2a1307c12c1302f0e3e394afcc7e647be
Ali Movahedi <ali_movahedi@aol.com>
2022-06-14 22:08:52 +0430

    refactor oepration call endpoint

--- src/endpoints/operation_call_builder.rs
+++ src/endpoints/operation_call_builder.rs
@@ -1,43 +1,40 @@
+use std::collections::HashMap;
+
+use crate::api_call::api_call;
 use crate::endpoints::{horizon::Record, CallBuilder, Server};
 use crate::types::Operation;
 use crate::utils::{req, Direction, Endpoint};
 
 #[derive(Debug)]
 pub struct OperationCallBuilder<'a> {
-    pub server: &'a Server,
-    pub cursor: Option<String>,
-    pub order: Option<Direction>,
-    pub limit: Option<u8>,
-    pub include_failed: bool,
-    pub endpoint: Endpoint,
+    server_url: &'a str,
+    endpoint: Endpoint,
+    query_params: HashMap<&'a str, &'a str>,
 }
 
 impl<'a> CallBuilder<'a, Operation> for OperationCallBuilder<'a> {
     fn new(s: &'a Server) -> Self {
         Self {
-            server: s,
-            cursor: None,
-            order: None,
-            limit: None,
-            include_failed: false,
+            server_url: &s.0,
             endpoint: Endpoint::None,
+            query_params: HashMap::new(),
         }
     }
 
-    fn cursor(&mut self, cursor: &str) -> &mut Self {
-        self.cursor = Some(String::from(cursor));
+    fn cursor(&mut self, cursor: &'a str) -> &mut Self {
+        self.query_params.insert("cursor", cursor);
 
         self
     }
 
     fn order(&mut self, dir: Direction) -> &mut Self {
-        self.order = Some(dir);
+        self.query_params.insert("order", dir.as_str());
 
         self
     }
 
     fn limit(&mut self, limit: u8) -> &mut Self {
-        self.limit = Some(limit);
+        self.query_params.insert("order", &limit.to_string());
 
         self
     }
@@ -48,38 +45,21 @@ impl<'a> CallBuilder<'a, Operation> for OperationCallBuilder<'a> {
         self
     }
 
-    fn call(&self) -> Result<Record<Operation>, &str> {
+    fn call(&self) -> Result<Record<Operation>, anyhow::Error> {
         let mut url = format!(
-            "{}{}{}&include_failed={}",
-            &self.server.0,
+            "{}{}{}",
+            &self.server_url,
             self.endpoint.as_str(),
-            "/operations?",
-            self.include_failed,
+            "/operations",
         );
 
-        if let Some(x) = &self.cursor {
-            url.push_str(&format!("&cursor={}", x));
-        }
-
-        if let Some(x) = &self.order {
-            url.push_str(&format!("&order={}", x.as_str()));
-        }
-
-        if let Some(x) = &self.limit {
-            url.push_str(&format!("&limit={}", x));
-        }
-
-        let resp = req(&url).unwrap();
-
-        let p: Record<Operation> = serde_json::from_str(&resp).unwrap();
-
-        Ok(p)
+        api_call::<Record<Operation>>(url, crate::types::HttpMethod::GET, self.query_params)
     }
 }
 
 impl<'a> OperationCallBuilder<'a> {
     pub fn include_failed(&mut self, i: bool) -> &mut Self {
-        self.include_failed = i;
+        self.query_params.insert("include_failed", &i.to_string());
 
         self
     }
@@ -93,7 +73,7 @@ mod tests {
     fn limit_operation_call_builder() {
         let s = Server::new(String::from("https://horizon.stellar.org"));
 
-        let mut ocb = OperationCallBuilder::new();
+        let mut ocb = OperationCallBuilder::new(&s);
 
         let op_records = ocb
             .for_endpoint(Endpoint::Accounts(String::from(
