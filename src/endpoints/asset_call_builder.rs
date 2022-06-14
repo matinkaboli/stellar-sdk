12995efe2ac4b349bc2c60c4ceaabe3e8b8a730d
Ali Movahedi <ali_movahedi@aol.com>
2022-06-14 21:03:57 +0430

    refactor asset call

--- src/endpoints/asset_call_builder.rs
+++ src/endpoints/asset_call_builder.rs
@@ -1,27 +1,26 @@
+use std::collections::HashMap;
+
+use crate::api_call::api_call;
 use crate::endpoints::{horizon::Record, CallBuilder, Server};
 use crate::types::AssetHorizon;
-use crate::utils::{req, Direction, Endpoint};
+use crate::utils::{Direction, Endpoint};
 
 #[derive(Debug)]
 pub struct AssetCallBuilder<'a> {
-    pub server: &'a Server,
-    pub cursor: Option<String>,
-    pub order: Option<Direction>,
-    pub limit: Option<u8>,
-    pub asset_code: Option<String>,
-    pub asset_issuer: Option<String>,
-    pub endpoint: Endpoint,
+    server_url: &'a str,
+    endpoint: Endpoint,
+    query_params: HashMap<&'a str, &'a str>,
 }
 
 impl<'a> AssetCallBuilder<'a> {
-    pub fn asset_code(&mut self, code: &str) -> &mut Self {
-        self.asset_code = Some(String::from(code));
+    pub fn asset_code(&mut self, code: &'a str) -> &mut Self {
+        self.query_params.insert("code", code);
 
         self
     }
 
-    pub fn asset_issuer(&mut self, issuer: &str) -> &mut Self {
-        self.asset_issuer = Some(String::from(issuer));
+    pub fn asset_issuer(&mut self, issuer: &'a str) -> &mut Self {
+        self.query_params.insert("issuer", issuer);
 
         self
     }
@@ -30,30 +29,26 @@ impl<'a> AssetCallBuilder<'a> {
 impl<'a> CallBuilder<'a, AssetHorizon> for AssetCallBuilder<'a> {
     fn new(s: &'a Server) -> Self {
         AssetCallBuilder {
-            server: s,
-            cursor: None,
-            order: None,
-            limit: None,
-            asset_code: None,
-            asset_issuer: None,
+            server_url: &s.0,
             endpoint: Endpoint::None,
+            query_params: HashMap::new(),
         }
     }
 
-    fn cursor(&mut self, cursor: &str) -> &mut Self {
-        self.cursor = Some(String::from(cursor));
+    fn cursor(&mut self, cursor: &'a str) -> &mut Self {
+        self.query_params.insert("cursor", cursor);
 
         self
     }
 
-    fn order(&mut self, o: Direction) -> &mut Self {
-        self.order = Some(o);
+    fn order(&mut self, dir: Direction) -> &mut Self {
+        self.query_params.insert("order", dir.as_str());
 
         self
     }
 
-    fn limit(&mut self, limit_number: u8) -> &mut Self {
-        self.limit = Some(limit_number);
+    fn limit(&mut self, limit: u8) -> &mut Self {
+        self.query_params.insert("order", &limit.to_string());
 
         self
     }
@@ -64,34 +59,15 @@ impl<'a> CallBuilder<'a, AssetHorizon> for AssetCallBuilder<'a> {
         self
     }
 
-    fn call(&self) -> Result<Record<AssetHorizon>, &str> {
-        let mut url = format!("{}{}{}", &self.server.0, self.endpoint.as_str(), "/assets?",);
+    fn call(&self) -> Result<Record<AssetHorizon>, anyhow::Error> {
+        let mut url = format!(
+            "{}{}{}",
+            &self.server_url,
+            self.endpoint.as_str(),
+            "/trades?",
+        );
 
-        if let Some(x) = &self.cursor {
-            url.push_str(&format!("&cursor={}", x));
-        }
-
-        if let Some(x) = &self.order {
-            url.push_str(&format!("&order={}", x.as_str()));
-        }
-
-        if let Some(x) = &self.limit {
-            url.push_str(&format!("&limit={}", x));
-        }
-
-        if let Some(x) = &self.asset_code {
-            url.push_str(&format!("&asset_code={}", x));
-        }
-
-        if let Some(x) = &self.asset_issuer {
-            url.push_str(&format!("&asset_issuer={}", x));
-        }
-
-        let resp = req(&url).unwrap();
-
-        let p: Record<AssetHorizon> = serde_json::from_str(&resp).unwrap();
-
-        Ok(p)
+        api_call::<Record<AssetHorizon>>(url, crate::types::HttpMethod::GET, self.query_params)
     }
 }
 
@@ -103,7 +79,7 @@ mod tests {
     fn assets_horizon_test() {
         let s = Server::new(String::from("https://horizon.stellar.org"));
 
-        let mut acb = AssetCallBuilder::new();
+        let mut acb = AssetCallBuilder::new(&s);
 
         let asset_records = acb
             .asset_issuer("GA5BUT4SND34VRUJGFEVLG6LMEYOU5HSSYZLX673I2IJVRLLPATMH4RN")
