731972c8807c301a0bc77bb4fb520aa7d96a97a0
Ali Movahedi <ali_movahedi@aol.com>
2022-06-17 23:44:11 +0430

    fix encode check

--- src/str_key.rs
+++ src/str_key.rs
@@ -51,12 +51,12 @@ impl Into<u8> for VersionBytes {
     }
 }
 
-fn encode_check(v: VersionBytes, data: &Vec<u8>) -> String {
+fn encode_check(v: VersionBytes, data: &mut Vec<u8>) -> String {
     let mut bytes: Vec<u8> = Vec::new();
     bytes.push(v.into());
-    bytes.append(&mut data.as_bytes().to_vec());
+    bytes.append(data);
 
-    let mut unencoded: Vec<u8> = Vec::new();
+    let mut unencoded: Vec<u8> = vec![0; 2];
 
     let crc: Crc<u16> = Crc::<u16>::new(&CRC_16_XMODEM);
     let checksum = crc.checksum(&bytes);
@@ -81,6 +81,18 @@ mod tests {
 
     #[test]
     fn test_buffer() {
-        encode_check(VersionBytes::Ed25519PublicKey, "ABCDE")
+        let encoded_public = encode_check(
+            VersionBytes::Ed25519PublicKey,
+            &mut vec![
+                91_u8, 49_u8, 118_u8, 218_u8, 79_u8, 232_u8, 118_u8, 216_u8, 114_u8, 82_u8, 9_u8,
+                175_u8, 17_u8, 217_u8, 95_u8, 50_u8, 155_u8, 52_u8, 15_u8, 112_u8, 137_u8, 99_u8,
+                101_u8, 172_u8, 40_u8, 104_u8, 207_u8, 154_u8, 154_u8, 33_u8, 113_u8, 92_u8,
+            ],
+        );
+
+        assert_eq!(
+            "GBNTC5W2J7UHNWDSKIE26EOZL4ZJWNAPOCEWGZNMFBUM7GU2EFYVZNOL",
+            encoded_public
+        );
     }
 }
