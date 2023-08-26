9364988f99ea68934bb1cb4fcb05002eabb0c3ee
palinko91 <arpad.palinkas@gmail.com>
2023-08-26 04:15:59 +0200

    More detailed SubmitTransactionResponse

--- src/types/submit_transaction_response.rs
+++ src/types/submit_transaction_response.rs
@@ -1,12 +1,91 @@
 use serde::{Deserialize, Serialize};
+// https://developers.stellar.org/api/horizon/resources/submit-a-transaction
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct SubmitTransactionResponse {
-    hash: String,
-    ledger: u64,
-    successful: bool,
-    envelope_xdr: String,
-    result_xdr: String,
-    result_meta_xdr: String,
-    paging_token: String,
+    pub memo: Option<String>,
+    pub memo_bytes: Option<String>,
+    #[serde(rename = "_links")]
+    pub links: Links,
+    pub id: String,
+    pub paging_token: String,
+    pub successful: bool,
+    pub hash: String,
+    pub ledger: i32,
+    pub created_at: String,
+    pub source_account: String,
+    pub account_muxed: Option<String>,
+    pub account_muxed_id: Option<String>,
+    pub source_account_sequence: String,
+    pub fee_account: String,
+    pub fee_account_muxed: Option<String>,
+    pub fee_account_muxed_id: Option<String>,
+    pub fee_charged: String,
+    pub max_fee: String,
+    pub operation_count: i32,
+    pub envelope_xdr: String,
+    pub result_xdr: String,
+    pub result_meta_xdr: String,
+    pub fee_meta_xdr: Option<String>,
+    pub memo_type: String,
+    pub signatures: Vec<String>,
+    pub valid_after: Option<String>,
+    pub valid_before: Option<String>,
+    pub preconditions: Option<Preconditions>,
+    pub fee_bump_transaction: Option<FeeBumpTransaction>,
+    pub inner_transaction: Option<InnerTransaction>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Links {
+    #[serde(rename = "self")]
+    pub links_self: Link,
+    pub account: Link,
+    pub ledger: Link,
+    pub operations: Link,
+    pub effects: Link,
+    pub precedes: Link,
+    pub succeeds: Link,
+    pub transaction: Link,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Link {
+    pub href: String,
+    pub templated: Option<bool>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Preconditions {
+    pub timebounds: Option<TimeboundsString>,
+    pub ledgerbounds: Option<Ledgerbounds>,
+    pub min_account_sequence: Option<String>,
+    pub min_account_sequence_age: Option<String>,
+    pub min_account_sequence_ledger_gap: Option<i32>,
+    pub extra_signers: Option<Vec<String>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Ledgerbounds {
+    pub min_ledger: Option<String>,
+    pub max_ledger: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct TimeboundsString {
+    pub min_time: Option<String>,
+    pub max_time: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FeeBumpTransaction {
+    pub hash: Option<String>,
+    pub signatures: Option<Vec<String>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct InnerTransaction {
+    pub hash: Option<String>,
+    pub signatures: Option<Vec<String>>,
+    pub max_fee: Option<String>,
 }
